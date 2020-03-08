df0871baf36527b655bd41e5d16af9220cf4ee86
Adam Kendis <atkendis@gmail.com>
2020-03-08 11:56:44 -0700

    Header component integration test.

---
+++ tests/integration/components/header-test.js
@@ -0,0 +1,14 @@
+import { module, test } from 'qunit';
+import { setupRenderingTest } from 'ember-qunit';
+import { render } from '@ember/test-helpers';
+import { hbs } from 'ember-cli-htmlbars';
+
+module('Integration | Component | header', function(hooks) {
+  setupRenderingTest(hooks);
+
+  test('it renders three links', async function(assert) {
+    await render(hbs`<Header />`);
+
+    assert.dom('a').exists({ count: 3 });
+  });
+});
