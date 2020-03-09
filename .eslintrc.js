56b58d1ddefc897fa51f46895e62f23def9e783e
Adam Kendis <atkendis@gmail.com>
2020-03-09 16:04:52 -0700

    Updated eslint rule.

--- .eslintrc.js
+++ .eslintrc.js
@@ -47,7 +47,8 @@ module.exports = {
 
         // this can be removed once the following is fixed
         // https://github.com/mysticatea/eslint-plugin-node/issues/77
-        'node/no-unpublished-require': 'off'
+        'node/no-unpublished-require': 'off',
+        'ember/avoid-leaking-state-in-ember-objects': 'off',
       })
     }
   ]
