b151060fa15871825ecc67e2801a23027fa9bac9
Adam Kendis <atkendis@gmail.com>
2020-03-13 17:07:23 -0700

    Controller:new unit tests for error handling and datestring formatting.

--- tests/unit/controllers/new-test.js
+++ tests/unit/controllers/new-test.js
@@ -16,10 +16,29 @@ module('Unit | Controller | new', function(hooks) {
   });
 
   test('it fails to step forward if no artist is selected', function(assert) {
+    assert.expect(2)
     let controller = this.owner.lookup('controller:new');
     controller.set('artistProps', { id: undefined });
     assert.equal(controller.currentStep, 'artist');
     controller.send('stepForm');
     assert.equal(controller.currentStep, 'artist');
   });
+
+  test('it displays an error if form field is incomplete', function(assert) {
+    assert.expect(3);
+    let controller = this.owner.lookup('controller:new');
+    controller.set('artistProps', { id: undefined });
+    assert.equal(controller.errors, undefined);
+    controller.send('stepForm');
+    assert.equal(controller.errors.length, 1);
+    assert.equal(controller.errors[0], 'Please make a selection');
+  });
+
+  test('formatDateString helper properly reformats date string', function(assert) {
+    assert.expect(1);
+    let controller = this.owner.lookup('controller:new');
+    let releaseDate = '1999-12-22'
+    let formattedDate = controller.formatDateString(releaseDate);
+    assert.equal(formattedDate, '12/22/1999')
+  })
 });
