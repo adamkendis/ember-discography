7ad014d8f4127e5f9809ea94dcfaf4a89f6fe0bc
Adam Kendis <atkendis@gmail.com>
2020-03-14 17:46:14 -0700

    Filter component.

---
+++ app/components/filter.js
@@ -0,0 +1,26 @@
+import Component from '@glimmer/component';
+import { tracked } from '@glimmer/tracking';
+
+export default class FilterComponent extends Component {
+  @tracked query = '';
+  
+  get placeholder() {
+    return `Search by ${this.args.filterKey}`;
+  }
+
+  get results() {
+    let { resources, filterKey } = this.args;
+    let query = this.query;
+
+    if (query) {
+      resources = resources.filter(resource => {
+        let normalized;
+        if (typeof resource[filterKey]  === 'string') {
+          normalized = resource[filterKey].toLowerCase();
+          return normalized.includes(query);
+        } 
+      })
+    }
+    return resources;
+  }
+}
