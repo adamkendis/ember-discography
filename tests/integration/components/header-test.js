6d36ab19df4ad33cc2783e34323068355b877542
Adam Kendis <atkendis@gmail.com>
2020-03-17 15:05:59 -0700

    Fixed incorrect test string for header integration.

--- tests/integration/components/header-test.js
+++ tests/integration/components/header-test.js
@@ -6,7 +6,7 @@ import { hbs } from 'ember-cli-htmlbars';
 module('Integration | Component | header', function(hooks) {
   setupRenderingTest(hooks);
 
-  test('it renders five links', async function(assert) {
+  test('it renders four links', async function(assert) {
     assert.expect(1);
     await render(hbs`<Header />`);
 
