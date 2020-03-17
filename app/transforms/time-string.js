2ffb73aa31cb3b6c835fd5e0c67e3921f647d084
Adam Kendis <atkendis@gmail.com>
2020-03-17 15:18:31 -0700

    Comments.

--- app/transforms/time-string.js
+++ app/transforms/time-string.js
@@ -1,6 +1,8 @@
 import Transform from '@ember-data/serializer/transform';
 
 export default class TimeStringTransform extends Transform {
+
+  // Converts integer (time in seconds) into MM:ss string.
   deserialize(serialized) {
     let minutes = Math.floor(serialized / 60);
     let seconds = serialized % 60;
@@ -8,6 +10,7 @@ export default class TimeStringTransform extends Transform {
     return `${minutes}:${seconds}`;
   }
 
+  // Converts MM:ss string into integer (time in seconds);
   serialize(deserialized) {
     let [minutes, seconds] = deserialized.split(':');
     return parseInt(minutes) * 60 + parseInt(seconds);
