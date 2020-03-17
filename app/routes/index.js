7de76207720179b258004abb66fd6427337c93a6
Adam Kendis <atkendis@gmail.com>
2020-03-17 00:03:52 -0700

    Updated index route to fetch artists.

--- app/routes/index.js
+++ app/routes/index.js
@@ -3,9 +3,9 @@ import { inject as service } from '@ember/service';
 
 export default class IndexRoute extends Route {
   @service store;
+  templateName = 'artists';
 
   async model() {
-    // return this.store.findAll('artist');
-    return 'hey!'
+    return this.store.findAll('artist', { include: 'albums' });
   }
 }
