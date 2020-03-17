110372846ba731a87973403404f101cc7150e29c
Adam Kendis <atkendis@gmail.com>
2020-03-17 15:07:50 -0700

    Added beforeModel hook to index route to transition to artists.

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,11 +1,8 @@
 import Route from '@ember/routing/route';
-import { inject as service } from '@ember/service';
 
 export default class IndexRoute extends Route {
-  @service store;
-  templateName = 'artists';
 
-  async model() {
-    return this.store.findAll('artist', { include: 'albums' });
+  beforeModel() {
+    this.transitionTo('artists');
   }
 }
